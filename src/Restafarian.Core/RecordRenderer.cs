d888b20c27c26ff0ca72da048c784a67464003e5
Cédric L. Charlier <seddryck@gmail.com>
2025-05-29 09:31:41 +0200

    feat: render model for resources as records

---
+++ src/Restafarian.Core/RecordRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Restafarian.Core.ApiSpecs;
+using Scriban;
+
+namespace Restafarian.Core;
+public static class RecordRenderer
+{
+    public static string RenderRecord(Model model, string ns)
+    {
+        var templateText = EmbeddedResourceLoader.Load("Templates/Record.sbncs");
+
+        var template = Template.Parse(templateText);
+        if (template.HasErrors)
+            throw new InvalidOperationException("Scriban template error: " + string.Join(", ", template.Messages));
+
+        var rendered = template.Render(new { @namespace = ns, model });
+        return rendered;
+    }
+}
