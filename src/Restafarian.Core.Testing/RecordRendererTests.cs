273a845af802bae7e35cb54504c4b3f41fa6334b
Cédric L. Charlier <seddryck@gmail.com>
2025-05-29 16:22:29 +0200

    feat: render model for resources as records (#3)
    * feat: render model for resources as records
    
    * docs: review icons

---
+++ src/Restafarian.Core.Testing/RecordRendererTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Legacy;
+using NUnit.Framework;
+using Restafarian.Core.ApiSpecs;
+
+namespace Restafarian.Core.Testing;
+[TestFixture]
+public class RecordRendererTests
+{
+    [Test]
+    public void RenderRecord_GeneratesBasicRecordCorrectly()
+    {
+        var model = new Model
+        {
+            Name = "Customer",
+            Fields =
+            [
+                new Field { Name = "id", Type = "int" },
+                new Field { Name = "name", Type = "string" }
+            ]
+        };
+
+        var result = RecordRenderer.RenderRecord(model, "MyNamespace");
+
+        Assert.That(result, Does.Contain("public record Customer"));
+        Assert.That(result, Does.Contain("public int Id { get; init; }"));
+        Assert.That(result, Does.Contain("public string Name { get; init; }"));
+        Assert.That(result, Does.Contain("namespace MyNamespace"));
+    }
+
+    [Test]
+    public void RenderRecord_SupportsNullableField()
+    {
+        var model = new Model
+        {
+            Name = "User",
+            Fields = [new Field { Name = "email", Type = "string", Nullable = true }]
+        };
+
+        var result = RecordRenderer.RenderRecord(model, "MyApp.Models");
+
+        Assert.That(result, Does.Contain("public string? Email { get; init; }"));
+    }
+
+    [Test]
+    public void RenderRecord_UsesCapitalizedPropertyNames()
+    {
+        var model = new Model
+        {
+            Name = "Thing",
+            Fields = [new Field { Name = "some_value", Type = "bool" }]
+        };
+
+        var result = RecordRenderer.RenderRecord(model, "Stuff");
+
+        Assert.That(result, Does.Contain("public bool Some_value { get; init; }"));
+    }
+}
---
+++ src/Restafarian.Core/EmbeddedResourceLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restafarian.Core;
+internal class EmbeddedResourceLoader
+{
+    public static string Load(string resourceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        using var stream = assembly.GetManifestResourceStream($"{typeof(EmbeddedResourceLoader).Namespace}.{resourceName.Replace("/", ".")}")
+                                ?? throw new FileNotFoundException($"Embedded resource not found: {resourceName}");
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+}
